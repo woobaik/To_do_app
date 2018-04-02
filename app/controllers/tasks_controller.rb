b05e09104f5621342a8fb6d01134d786fef67fd2
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-04-02 14:55:36 -0700

    ADD index and new page and controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,22 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_param)
+    if @task.save
+      redirect_to tasks_index_path
+    end
+  end
+
+  private
+
+  def task_param
+    params.require(:task).permit(:title, :description)
   end
 end
