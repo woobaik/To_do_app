b950c2329913e3e3abd3c35c6f728f68bacba04c
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-04-07 00:57:38 -0400

    ADD session part 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to :tasks
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
